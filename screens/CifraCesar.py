99a3ba4b66f1adc6128aa5ad541a51619e27559f
Guilherme N. Marques <guilherme.n.marques@unesp.br>
2026-04-02 20:46:51 -0300

    Screens(fix): criando uma classe base para todas as criptografias.

--- screens/CifraCesar.py
+++ screens/CifraCesar.py
@@ -1,41 +1,22 @@
 import customtkinter as ctk
-
-class CesarFrame(ctk.CTkFrame):
+from screens.BaseScreen import BaseScreen
+class CesarFrame(BaseScreen):
     def __init__(self, master, cifra_class, callback_voltar):
-        super().__init__(master)
-        self.cifra_instancia = cifra_class() # Instancia a Cifra de César
-        self.callback_voltar = callback_voltar
+    
+        super().__init__(master, cifra_class(), callback_voltar)
 
-      
-        self.label_titulo = ctk.CTkLabel(self, text=self.cifra_instancia.NOME, font=("Arial", 24, "bold"))
-        self.label_titulo.pack(pady=(20, 5))
-      
-        self.input_text = ctk.CTkTextbox(self, height=100)
-        self.input_text.pack(fill="x", padx=20, pady=10)
-        self.input_text.insert("0.0", "Digite sua mensagem aqui...")
+        self._build_input_common_components()
 
        
-        self.setup_controles()
+        self._build_particular_components()
 
       
-        self.output_text = ctk.CTkTextbox(self, height=100, state="disabled")
-        self.output_text.pack(fill="x", padx=20, pady=10)
+        self._build_output_common_components()
 
      
-        self.btn_container = ctk.CTkFrame(self, fg_color="transparent")
-        self.btn_container.pack(pady=20)
-
-        self.btn_voltar = ctk.CTkButton(self.btn_container, text="Voltar", command=self.callback_voltar, fg_color="gray30")
-        self.btn_voltar.pack(side="left", padx=10)
-
-        self.btn_executar = ctk.CTkButton(self.btn_container, text="Criptografar", command=self.processar)
-        self.btn_executar.pack(side="left", padx=10)
+       
         
-        self.btn_executar = ctk.CTkButton(self.btn_container, text="Descriptografar", command=lambda: self.processar(False))
-        self.btn_executar.pack(side="left", padx=10)
-        
-
-    def setup_controles(self):
+    def _build_particular_components(self):
         """Cria o seletor numérico para o Shift da Cifra de César"""
         frame_shift = ctk.CTkFrame(self, fg_color="transparent")
         frame_shift.pack(pady=10)
@@ -44,8 +25,9 @@ class CesarFrame(ctk.CTkFrame):
         self.entry_shift = ctk.CTkEntry(frame_shift, width=50)
         self.entry_shift.insert(0, "3")
         self.entry_shift.pack(side="left")
+        
 
-    def processar(self, criptografar: bool = True):
+    def _processar(self, criptografar: bool = True, **kwargs):
         self.output_text.configure(state="normal")
         texto_puro = self.input_text.get("1.0", "end-1c")
         
